78602fdc810f819043241b36e6be384cd42ae0a7
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-12-06 00:06:42 -0300

    message

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-const mosca = require("mosca");
-// configurações do broken
-const settings = {
-    port: 3030
+const mosca = require('mosca'); // Importa o modulo Mosca
+const settings =require('./settings'); // configurações
+// Pode ser integrado a banco de dados como: Redis e MongoDB
+const server = new mosca.Server(settings); // Cria um Broker MQTT com base nas configurações
+
+server.on('clientConnected', function (cliente) { // Evento: ocorre quando um novo cliente se conecta ao Broker
+    console.log('Cliente Conectado', cliente.id); // Exibe uma mensagem com o ID do cliente conectado
+});
+
+server.on('published', function (packet, client) { //  Evento: ocorre quando uma mensagem é Puplicada
+    console.log( "Tópico: ", packet.topic, "|Mensagen :",packet.payload.toString(), " | ", new Date().toISOString()); // Exibe uma mensagem com o tópico da mensagem recebida
+    try {
+        const data =JSON.parse(packet.payload.toString());
+        console.log(data);
+    } catch(e) {
+        const log = {
+            status: false,
+            code: 500,
+            mensage: "Não foi possivel ler a mensagen",
+            // err: e
+        }
+    }
+});
+
+server.on('clientDisconnected', function (cliente) {
+    console.log('Cliente Desconectado: ', cliente.id);
+});
+
+server.on('ready', setup); // Inicio do Broker
+function setup() {
+    const log = {
+        status: true,
+        mensage: "Server run",
+        port: settings.port
+    };
+    console.log(log);
 }
-
-// criação do servidor
-const server = new mosca.Server(settings);
-
-// mensagen exibida ao iniciar o server
-server.on('ready', () => console.log('server on'));
-
