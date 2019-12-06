df838dea2e62f7e33d3dae87be3027f1b237d5d0
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-12-06 16:40:11 -0300

    setting log structure

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 const mosca = require('mosca'); // Importa o modulo Mosca
 const settings =require('./settings'); // configurações
-
+const Log = require('./log'); // Estrutura de log para trabalhar com as callbacks
 // Pode ser integrado a banco de dados como: Redis e MongoDB
 const broker = new mosca.Server(settings); // Cria um Broker MQTT com base nas configurações
 
 // Evento: ocorre quando um novo cliente se conecta ao Broker
-broker.on('clientConnected', function (cliente) { 
+broker.on('clientConnected', (client, error) => { 
     // Exibe uma mensagem com o ID do cliente conectado
-    console.log('Cliente Conectado', cliente.id); 
+    const log = Log({client: client.id, topic: client.topic});
+    // tratamento de error
+    if (error) {
+        log.code = 200;
+        log.status = "Error";
+        log.error = error;
+        log.message = "Não foi possivel fazer subscribe";
+        console.log(`\nType: Subscribe Status: ${log.status} Code: ${log.code} Message: ${log.message} Error: ${log.error}`);
+    } else {
+        log.message = "Cadastro realizado com sucesso";
+    }
+    console.log(`\nType: Subscribe Status: ${log.status} Code: ${log.code} Message: ${log.message}`);
 });
 
 //  Evento: ocorre quando uma mensagem é Puplicada
-broker.on('published', function (packet, client) { 
+broker.on('published', function (packet, client,message) { 
     // Exibe uma mensagem com o tópico da mensagem recebida
-    console.log( "Tópico: ", packet.topic, "|Mensagen :",packet.payload.toString(), " | ", new Date().toISOString()); 
-    // tratando para trabalhar em json
-    try {
-        const data =JSON.parse(packet.payload.toString());
-        console.log(data);
-    } catch(e) {
-        const log = {
-            status: false,
-            code: 500,
-            mensage: "Não foi possivel ler a mensagen",
-            // err: e
-        }
-    }
+    const log = Log({
+        packet: packet.payload.toString(),
+        mensage: message
+    });
+    console.log(`\nType: Pulish Status: ${log.status} Code: ${log.code} Payload: ${log.packet}`);
 });
 
-broker.on('clientDisconnected', function (cliente) {
-    console.log('Cliente Desconectado: ', cliente.id);
+broker.on('clientDisconnected', function (client,error) {
+        // Exibe uma mensagem com o ID do cliente conectado
+        const log = Log({client: client.id, topic: client.topic});
+        // tratamento de error
+        if (error) {
+            log.code = 200;
+            log.status = "Error";
+            log.error = error;
+            log.message = "Não foi possivel fazer subscribe";
+            console.log(`\nType: Subscribe Status: ${log.status} Code: ${log.code} Message: ${log.message} Error: ${log.error}`);
+        } else {
+            log.message = "Cadastro realizado com sucesso";
+        }
+        console.log(`\nType: Subscribe Status: ${log.status} Code: ${log.code} Message: ${log.message}`);
+    // console.log('Cliente Desconectado: ', cliente.id);
 });
 
 // Inicio do Broker
