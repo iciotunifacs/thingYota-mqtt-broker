7a25269054ab06b2ac3aa188de7df065729b854d
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-12-06 18:37:02 -0300

    create auth

--- log.js
+++ log.js
@@ -1,19 +1,26 @@
+/**
+ * @author Victor Raton
+ * @version 1.0
+ * @description Gerar modelo padrão de log
+ * @param {Object*} args
+ * @returns {Object* Log} 
+ * @example var log = Log({username: "username", code: 10})
+ */
 const Log = (args) => {
+    // that: padrão de logbase
     const that = {
         status : "OK",
         code : 500,
         message: "",
         timestamp: new Date().toUTCString(),
     };
-    
+    // lista de chaves dos objetos passados en args
     const keys = Object.keys(args);
-    
+    // mapeqmento de parâmetros adicionais no log
     keys.map(el => {
         const value = args[el];
         that[el] = value;
     })
-    
     return that;
 }
-
 module.exports = Log;
