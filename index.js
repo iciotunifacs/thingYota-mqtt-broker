e71654735e13ea5471d0da14b6b2b7e3a2a32dd9
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-12-06 00:57:34 -0300

    message

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const mosca = require('mosca'); // Importa o modulo Mosca
 const settings =require('./settings'); // configurações
-// Pode ser integrado a banco de dados como: Redis e MongoDB
-const server = new mosca.Server(settings); // Cria um Broker MQTT com base nas configurações
 
-server.on('clientConnected', function (cliente) { // Evento: ocorre quando um novo cliente se conecta ao Broker
-    console.log('Cliente Conectado', cliente.id); // Exibe uma mensagem com o ID do cliente conectado
+// Pode ser integrado a banco de dados como: Redis e MongoDB
+const broker = new mosca.Server(settings); // Cria um Broker MQTT com base nas configurações
+
+// Evento: ocorre quando um novo cliente se conecta ao Broker
+broker.on('clientConnected', function (cliente) { 
+    // Exibe uma mensagem com o ID do cliente conectado
+    console.log('Cliente Conectado', cliente.id); 
 });
 
-server.on('published', function (packet, client) { //  Evento: ocorre quando uma mensagem é Puplicada
-    console.log( "Tópico: ", packet.topic, "|Mensagen :",packet.payload.toString(), " | ", new Date().toISOString()); // Exibe uma mensagem com o tópico da mensagem recebida
+//  Evento: ocorre quando uma mensagem é Puplicada
+broker.on('published', function (packet, client) { 
+    // Exibe uma mensagem com o tópico da mensagem recebida
+    console.log( "Tópico: ", packet.topic, "|Mensagen :",packet.payload.toString(), " | ", new Date().toISOString()); 
+    // tratando para trabalhar em json
     try {
         const data =JSON.parse(packet.payload.toString());
         console.log(data);
@@ -22,11 +28,12 @@ server.on('published', function (packet, client) { //  Evento: ocorre quando uma
     }
 });
 
-server.on('clientDisconnected', function (cliente) {
+broker.on('clientDisconnected', function (cliente) {
     console.log('Cliente Desconectado: ', cliente.id);
 });
 
-server.on('ready', setup); // Inicio do Broker
+// Inicio do Broker
+broker.on('ready', setup); 
 function setup() {
     const log = {
         status: true,
