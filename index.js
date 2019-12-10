04dc35757fc4d1d82f80f36e7f61a0702d589d56
Victor <vfbraton@gmail.com>
2019-12-10 11:49:14 -0300

    fix read topic and log

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mosca = require('mosca'); // Importa o modulo Mosca
-const brokerSettings =require('./broker-settings'); // configurações
+const brokerSettings = require('./broker-settings'); // configurações
 const Logger = require('./log'); // Estrutura de log para trabalhar com as callbacks
 const Log = Logger.construct;
 const showLog = Logger.print;
@@ -11,9 +11,9 @@ const mongodb = require('./mongodb');
 const broker = new mosca.Server(brokerSettings); // Cria um Broker MQTT com base nas configurações
 
 // Evento: ocorre quando um novo cliente se conecta ao Broker
-broker.on('clientConnected', (client, error) => { 
+broker.on('clientConnected', (client, error) => {
     // Exibe uma mensagem com o ID do cliente conectado
-    const log = Log({client: client.id.toString()});
+    const log = Log({ client: client.id.toString() });
 
     // tratamento de error
     if (error) {
@@ -28,36 +28,38 @@ broker.on('clientConnected', (client, error) => {
 });
 
 //  Evento: ocorre quando uma mensagem é Puplicada
-broker.on('published', function (packet, client,message) { 
+broker.on('published', function (packet, client, message) {
+    console.log(packet.topic);
+    console.log(packet.payload);
     // Exibe uma mensagem com o tópico da mensagem recebida
-    const log = Log({ packet: packet.payload.toString(), Type: "Publish"});
+    // const log = Log({ data: packet.payload.toString(), Type: "Publish" });
     // TODO: configurar mensagen
     // mostra message se tiver alguma
-    message || message !== undefined ? log.message = message : log.message = "void";
-    console.log(showLog(log));
+    // message || message !== undefined ? log.message = message : log.message = "void";
+    // console.log(showLog(log));
 });
 
-broker.on('clientDisconnected', function (client,error) {
-        // Exibe uma mensagem com o ID do cliente conectado
-        const log = Log({client: client.id, topic: client.topic});
-        // tratamento de error
-        if (error) {
-            log.code = 200;
-            log.status = "Error";
-            log.error = error;
-            log.message = "Não foi possivel fazer unsubscribe";
-        } else {
-            log.message = "Desconexão realizado com sucesso";
-        }
-        console.log(showLog(log));
+broker.on('clientDisconnected', function (client, error) {
+    // Exibe uma mensagem com o ID do cliente conectado
+    const log = Log({ client: client.id, topic: client.topic });
+    // tratamento de error
+    if (error) {
+        log.code = 200;
+        log.status = "Error";
+        log.error = error;
+        log.message = "Não foi possivel fazer unsubscribe";
+    } else {
+        log.message = "Desconexão realizado com sucesso";
+    }
+    // console.log(showLog(log));
 });
 
 
 // Inicio do Broker
-broker.on('ready', setup); 
+broker.on('ready', setup);
 function setup() {
-    const log = Log({status: true, mensage: "Server run", port: brokerSettings.port});
-    console.log(showLog(log));
+    const log = Log({ status: true, mensage: "Server run", port: brokerSettings.port });
+    // console.log(showLog(log));
     let nmessage = {
         topic: '/hello/world',
         payload: 'abcde', // or a Buffer
